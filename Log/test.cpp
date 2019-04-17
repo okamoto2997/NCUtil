8c43b1005b72d33015e81b45f7060abe24ab3e85
Mamoru OKAMOTO <okamoto6496@gmail.com>
2019-04-17 20:54:42 +0900

    test updated

--- Log/test.cpp
+++ Log/test.cpp
@@ -2,7 +2,15 @@
 #include <iostream>
 
 int main(){
-
-	std::cerr << "test  !!!\n";
+	using namespace NCUtils::Log;
+	LineStreamBuffer bf(std::cout);
+	std::ostream dest(&bf);
+	dest << "test  !!!\n";
+	dest << "test  !!!";
+	dest << "test  !!!\n";
+	dest << "ujyfg";
+ 	dest << std::flush;
+	dest << "ujyfpppg";
+	dest << std::endl;
 	return 0;
 }
